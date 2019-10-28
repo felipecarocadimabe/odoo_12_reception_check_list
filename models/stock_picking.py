ea0a1a4c2b75df1c31f88f793b850b64d2c83fb6
felipecarocadimabe <felipe.caroca@dimabe.cl>
2019-10-28 14:21:27 -0300

    v2

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -2,28 +2,32 @@ from odoo import models, fields, api
 
 
 class StockPicking(models.Model):
-
     _inherit = 'stock.picking'
 
     check_list_responses = fields.One2many(
         'check.list.response',
         'picking_id',
-        'check list'
+        'Check List'
     )
 
     @api.model
     def create(self, val_list):
+        items = self.get_all_check_list_items()
 
-        check_list_items = self.env['check.list.item']
+        picking = super(StockPicking, self).create(val_list)
 
-        picking = super(self).create(val_list)
         responses = []
-        for item in check_list_items:
+        for item in items:
             responses.append({
-                'item': item,
-                'picking_id': picking
+                'item': item.id,
+                'picking_id': picking.id
             })
 
         self.env['check.list.response'].create(responses)
 
         return picking
+
+    @api.model
+    def get_all_check_list_items(self):
+        items = self.env['check.list.item']
+        return items.search([])
